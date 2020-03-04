3349f8b042e5ddd3c0ea878d018ee6be3f5135bf
Aditya Verma <Aditya.Verma8251@gmail.com>
2020-03-04 19:52:22 +0530

    Basic working code

--- LongestSubstring/src/Solution.java
+++ LongestSubstring/src/Solution.java
@@ -1,5 +1,20 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        return 0;
+        char[] characters = s.toCharArray();
+        int max = 0;
+        for(int i = 0; i < s.length(); i++){
+            String solution = "";
+            for (int j = i; j < s.length(); j++){
+                if (solution.contains(Character.toString(s.charAt(j)))) {
+                    break;
+                } else {
+                    solution = solution.concat(Character.toString(s.charAt(j)));
+                }
+                int l = solution.length();
+                if (l > max)
+                    max = l;
+            }
+        }
+        return max;
     }
 }
